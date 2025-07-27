57be9024a9e506090d7c004cba5f8b713b5c15c7
Mohamed Nabil Ouf <104600074+Mohamedouf125@users.noreply.github.com>
2025-07-27 14:05:14 +0300

    feat(footer): enhance social media links and navigation in footer
    - Add social media links for Facebook, Instagram, LinkedIn, YouTube, and TikTok with appropriate icons.
    - Update navigation links for "About Us," "Contact Us," "Services," and "Projects" to use anchor tags for better accessibility and styling.
    - Improve overall layout and styling for a more cohesive footer design.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,41 +32,87 @@ const Footer = () => {
             </div>
 
             <div className="mt-[20px] justify-center w-full md:justify-start items-start gap-[22px] inline-flex">
-              <div className=" py-px justify-start items-start flex">
+              {/* Facebook */}
+              <a
+                href="https://www.facebook.com/Devunitysoftware"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-px justify-start items-start flex"
+              >
                 <div className="text-center text-white/60 text-lg font-normal font-['Font Awesome 5 Brands'] leading-[18px]">
-                  <svg
-                    width="19"
-                    height="15"
-                    viewBox="0 0 19 15"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16.6367 3.77344C16.6367 3.94922 16.6367 4.08984 16.6367 4.26562C16.6367 9.15234 12.9453 14.7422 6.16016 14.7422C4.05078 14.7422 2.11719 14.1445 0.5 13.0898C0.78125 13.125 1.0625 13.1602 1.37891 13.1602C3.10156 13.1602 4.68359 12.5625 5.94922 11.5781C4.33203 11.543 2.96094 10.4883 2.50391 9.01172C2.75 9.04688 2.96094 9.08203 3.20703 9.08203C3.52344 9.08203 3.875 9.01172 4.15625 8.94141C2.46875 8.58984 1.20312 7.11328 1.20312 5.32031V5.28516C1.69531 5.56641 2.29297 5.70703 2.89062 5.74219C1.87109 5.07422 1.23828 3.94922 1.23828 2.68359C1.23828 1.98047 1.41406 1.34766 1.73047 0.820312C3.55859 3.03516 6.30078 4.51172 9.35938 4.6875C9.28906 4.40625 9.25391 4.125 9.25391 3.84375C9.25391 1.80469 10.9062 0.152344 12.9453 0.152344C14 0.152344 14.9492 0.574219 15.6523 1.3125C16.4609 1.13672 17.2695 0.820312 17.9727 0.398438C17.6914 1.27734 17.1289 1.98047 16.3555 2.4375C17.0938 2.36719 17.832 2.15625 18.4648 1.875C17.9727 2.61328 17.3398 3.24609 16.6367 3.77344Z"
-                      fill="white"
-                      fillOpacity="0.6"
-                    />
+                  <svg width="19" height="19" fill="white" fillOpacity="0.6" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M279.14 288l14.22-92.66h-88.91V127.45c0-25.35 12.42-50.06 52.24-50.06H293V6.26S259.5 0 225.36 0C141.09 
+        0 89.09 54.42 89.09 153.42v68.92H0V288h89.09v224h107.77V288z" />
                   </svg>
                 </div>
-              </div>
-              <div className=" pl-[0.63px] pr-[2.37px] py-px justify-start items-start flex">
+              </a>
+
+              {/* Instagram */}
+              <a
+                href="https://www.instagram.com/devunity_software"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-px justify-start items-start flex"
+              >
                 <div className="text-center text-white/60 text-lg font-normal font-['Font Awesome 5 Brands'] leading-[18px]">
-                  <svg
-                    width="17"
-                    height="17"
-                    viewBox="0 0 17 17"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.3799 0.554688C15.9775 0.554688 16.5049 1.08203 16.5049 1.71484V15.1797C16.5049 15.8125 15.9775 16.3047 15.3799 16.3047H1.84473C1.24707 16.3047 0.754883 15.8125 0.754883 15.1797V1.71484C0.754883 1.08203 1.24707 0.554688 1.84473 0.554688H15.3799ZM5.50098 14.0547V6.56641H3.18066V14.0547H5.50098ZM4.34082 5.51172C5.0791 5.51172 5.67676 4.91406 5.67676 4.17578C5.67676 3.4375 5.0791 2.80469 4.34082 2.80469C3.56738 2.80469 2.96973 3.4375 2.96973 4.17578C2.96973 4.91406 3.56738 5.51172 4.34082 5.51172ZM14.2549 14.0547V9.94141C14.2549 7.9375 13.7979 6.35547 11.4424 6.35547C10.3174 6.35547 9.54395 6.98828 9.22754 7.58594H9.19238V6.56641H6.97754V14.0547H9.29785V10.3633C9.29785 9.37891 9.47363 8.42969 10.7041 8.42969C11.8994 8.42969 11.8994 9.55469 11.8994 10.3984V14.0547H14.2549Z"
-                      fill="white"
-                      fillOpacity="0.6"
-                    />
+                  <svg width="19" height="19" fill="white" fillOpacity="0.6" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M224,202.66A53.34,53.34,0,1,0,277.34,256,53.38,53.38,0,0,0,224,202.66Zm124.71-41a54,54,0,0,0-30.38-30.38C292.91,122.58,224,122.58,224,122.58s-68.91,0-94.33,8.7a54,54,0,0,0-30.38,30.38C86.62,147.09,86.62,176,86.62,176s0,68.91,8.7,94.33a54,54,0,0,0,30.38,30.38c25.42,8.7,94.33,8.7,94.33,8.7s68.91,0,94.33-8.7a54,54,0,0,0,30.38-30.38c8.7-25.42,8.7-94.33,8.7-94.33S357.41,147.09,348.71,161.66ZM224,338a82,82,0,1,1,82-82A82,82,0,0,1,224,338Zm85.33-148.5a19.17,19.17,0,1,1,19.17-19.17A19.17,19.17,0,0,1,309.33,189.5Z" />
                   </svg>
                 </div>
-              </div>
+              </a>
+
+              {/* LinkedIn */}
+              <a
+                href="https://www.linkedin.com/company/devunity-software/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-px justify-start items-start flex"
+              >
+                <div className="text-center text-white/60 text-lg font-normal font-['Font Awesome 5 Brands'] leading-[18px]">
+                  <svg width="19" height="19" fill="white" fillOpacity="0.6" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.7 0 54.5S24.09 1 53.79 1s53.8 24.4 53.8 
+        53.5-24.09 53.6-53.8 53.6zM447.9 448h-92.4V302.4c0-34.7-.7-79.4-48.4-79.4-48.4 0-55.8 37.8-55.8 
+        76.9V448h-92.5V148.9h88.9v40.8h1.3c12.4-23.4 42.5-48.4 87.4-48.4 93.5 0 110.7 61.5 110.7 
+        141.3V448z"/>
+                  </svg>
+                </div>
+              </a>
+
+              {/* YouTube */}
+              <a
+                href="https://www.youtube.com/channel/UCzwAsr3xXh2XvUqZSUB-CoA"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-px justify-start items-start flex"
+              >
+                <div className="text-center text-white/60 text-lg font-normal font-['Font Awesome 5 Brands'] leading-[18px]">
+                  <svg width="19" height="19" fill="white" fillOpacity="0.6" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M549.655 124.083c-6.281-23.65-24.854-42.213-48.5-48.5C465.03 64 288 64 288 
+        64S110.97 64 74.845 75.583c-23.65 6.287-42.219 24.85-48.5 48.5C14.117 160.208 14.117 256 14.117 
+        256s0 95.792 12.228 131.917c6.281 23.65 24.85 42.213 48.5 48.5C110.97 448 288 448 288 
+        448s177.03 0 213.155-11.583c23.65-6.287 42.219-24.85 48.5-48.5C561.883 351.792 561.883 
+        256 561.883 256s0-95.792-12.228-131.917zM232.1 338.096V173.904L361.818 256 232.1 338.096z"/>
+                  </svg>
+                </div>
+              </a>
+
+              {/* TikTok */}
+              <a
+                href="https://www.tiktok.com/@devunity_software"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-px justify-start items-start flex"
+              >
+                <div className="text-center text-white/60 text-lg font-normal font-['Font Awesome 5 Brands'] leading-[18px]">
+                  <svg width="19" height="19" fill="white" fillOpacity="0.6" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M448,209.9v76.7c-34.5,0-66.4-10.8-92.5-29.2v120.4c0,74.7-60.7,135.4-135.4,135.4S84.7,452.5,84.7,377.8
+        s60.7-135.4,135.4-135.4c5.5,0,10.9,0.4,16.2,1.2v78.4c-5.2-1.5-10.7-2.3-16.2-2.3c-31.5,0-57.2,25.6-57.2,57.2
+        s25.6,57.2,57.2,57.2s57.2-25.6,57.2-57.2V0h76.6c0,58.1,47.1,105.2,105.3,105.2V209.9z"/>
+                  </svg>
+                </div>
+              </a>
             </div>
+
 
             <div className="mt-[40px] justify-start items-start gap-2 inline-flex">
               <div className=" p-[15px] bg-white/10 rounded-xl shadow-[inset_0px_0px_0px_0px_rgba(0,153,255,1.00)] justify-start items-start flex">
@@ -89,27 +135,20 @@ const Footer = () => {
                 </div>
               </div>
               <div className="flex-col justify-start items-center md:items-start gap-3 flex">
-                <div className="self-stretch justify-start items-center flex-col md:items-start inline-flex">
-                  <div className="justify-center items-center flex text-white/60 text-base font-normal font-['Inter'] leading-normal">
-                    About Us
-                  </div>
-                </div>
-                <div className="self-stretch justify-start items-center flex-col md:items-start inline-flex">
-                  <div className="justify-center items-center flex text-white/60 text-base font-normal font-['Inter'] leading-normal">
-                    Contact Us
-                  </div>
-                </div>
-                <div className="self-stretch justify-start items-center flex-col md:items-start inline-flex">
-                  <div className="justify-center items-center flex text-white/60 text-base font-normal font-['Inter'] leading-normal">
-                    Services
-                  </div>
-                </div>
-                <div className="self-stretch justify-start items-center flex-col md:items-start inline-flex">
-                  <div className="justify-center items-center flex text-white/60 text-base font-normal font-['Inter'] leading-normal">
-                    Projects
-                  </div>
-                </div>
+                <a href="/about" className="text-white/60 hover:text-white transition text-base font-normal font-['Inter'] leading-normal">
+                  About Us
+                </a>
+                <a href="/contact" className="text-white/60 hover:text-white transition text-base font-normal font-['Inter'] leading-normal">
+                  Contact Us
+                </a>
+                <a href="/services" className="text-white/60 hover:text-white transition text-base font-normal font-['Inter'] leading-normal">
+                  Services
+                </a>
+                <a href="/projects" className="text-white/60 hover:text-white transition text-base font-normal font-['Inter'] leading-normal">
+                  Projects
+                </a>
               </div>
+
             </div>
 
             <div className="w-[44.25px] flex-col justify-start items-center md:items-start gap-[24px] inline-flex">
